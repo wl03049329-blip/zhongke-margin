1155d6daa38e1aebfb523781286dd1bf8625c618
wl03049329-blip <wl03049329@gmail.com>
2026-09-09 23:40:38 +0800

    升級試算器快速操作至 Version 3.4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='op-margin-v3.3';
+const CACHE='op-margin-v3.4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./op-logo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
